9ae0a16370545169aa00de6519ab154d86e560b7
Ethan Salisbury <ethan.salisbury@plexusworldwide.com>
2019-02-25 16:51:43 -0700

    Removed route specific middleware

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/', bodyParser.json());
-app.use('/emojis', bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.urlencoded({extend: false}));
 
 app.get('/', function(req, res){
     res.status(200).send({message: 'success'});
@@ -98,8 +98,6 @@ app.post('/timbow', function(req, res, next){
             'Content_Type': 'application/json'
         }
     }
-    console.log('BODY', req.body);
-    console.log('REQUEST OPTIONS', requestOptions)
 
     request.post(requestOptions, function(err, response){
         if (err){
